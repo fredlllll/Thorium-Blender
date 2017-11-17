32b290b2c86d0a226ce74228260f4a6df1147ed2
Frederik Gelder <frederik.gelder@freenet.de>
2017-11-18 00:18:55 +0100

    removing end slash, as it could(is?) confuse gsutil

--- Thorium-Blender/blender.sh
+++ Thorium-Blender/blender.sh
@@ -13,6 +13,6 @@ mkdir -p $workingDir
 
 $blender2_79 -b $blendPath -s $startFrame -e $endFrame -o $workingDir/ -s
 
-gsutil cp -r $workingDir gs://fredusarfs/frames/$jobId/
+gsutil cp -r $workingDir gs://fredusarfs/frames/$jobId
 
 rm -rf $workingDir
