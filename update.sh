22282be099525b21b87311e7b600f45d771b81d4
Frederik Gelder <frederik.gelder@freenet.de>
2017-11-17 13:45:49 +0100

    adding update script

--- update.sh
+++ update.sh
@@ -1,9 +1,14 @@
-git pull
+USER="thorium"
 
+su $USER <<EOM
+cd $PWD
+git fetch
+git pull
+git submodule update --init --recursive
+git submodule update --recursive --remote
 cd Thorium-Computing-Farm/Source
 nuget restore
-msbuild
 cd ../..
-
 nuget restore
 msbuild
+EOM
