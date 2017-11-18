ea90a1b49bf5055aee8ed9c65b0aed0bba28342c
Frederik Gelder <frederik.gelder@freenet.de>
2017-11-18 20:10:11 +0100

    ; before then

--- Thorium-Blender/blender.sh
+++ Thorium-Blender/blender.sh
@@ -14,8 +14,8 @@ mkdir -p $workingDir
 
 $blender2_79 -b $blendPath -s $startFrame -e $endFrame -o $workingDir/ -a
 
-if [ $uploadType == "Async" ] then
-	(gsutil -m cp -r $workingDir/* gs://fredusarfs/frames/$jobId/ ; rm -rf $workingDir)&
+if [ $uploadType == "Async" ]; then
+	(gsutil -m cp -r $workingDir/* gs://fredusarfs/frames/$jobId/ ; rm -rf $workingDir) &
 else
     gsutil -m cp -r $workingDir/* gs://fredusarfs/frames/$jobId/
     rm -rf $workingDir
